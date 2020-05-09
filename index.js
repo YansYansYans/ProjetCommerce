4b88087df9c78107370eb454e6b6c94af9557b17
Aitkhelifa Yannis <yan95100@gmail.com>
2020-05-09 21:06:30 +0200

    Projet Test Panier 1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
     // Liste des articles dans le panier
     var itemCost = [];
 
+
 // Ajouter un article
     function add(n){
         // n = rang de la camera
@@ -75,30 +76,80 @@
     document.getElementById("total").innerHTML = "Total: " + total.toFixed(2) + " €"; 
     }
 
-    var art1 = document.getElementById('art1');
-    art1.onclick = function(){
-        var carre = document.querySelector('.fed');
-        carre.classList.add('fedvisible')
-    }
-    var art1 = document.getElementById('art2');
-    art1.onclick = function(){
-        var carre = document.querySelector('.anex');
-        carre.classList.add('anexvisible')
-    }
-    var art1 = document.getElementById('art3');
-    art1.onclick = function(){
-        var carre = document.querySelector('.stramer');
-        carre.classList.add('stramervisible')
-    }
-    var art1 = document.getElementById('art4');
-    art1.onclick = function(){
-        var carre = document.querySelector('.mamiya');
-        carre.classList.add('mamiyavisible')
-    }
-    var art1 = document.getElementById('art5');
-    art1.onclick = function(){
-        var carre = document.querySelector('.canon');
-        carre.classList.add('canonvisible')
-    }
+    class Camera {
+            constructor(id, name, price, description, imageUrl) {
+                this.id = id;
+                this.name = name;
+                this.price = price;
+                this.description = description;
+                this.imageUrl = imageUrl;
+            }
+            showTotal() {
+                console.log('Total : ' + this.price + ' € ')
+            }
+        }
+
+        var id = document.querySelector('id');
+
+        // liste des articles
+        let Camera1 = new Camera(
+            "name1",
+            "Camera FED 3",
+            48,
+            "La FED 3 a été crée en 1967, c'est le premier reflex numerique. Il connait un succet fou dans les années 70' après que sa promotion dans une pub avec Mickael Jordan.",
+            "images/vcam_1.jpg");
+        let Camera2 = new Camera(
+            "name2",
+            "Camera Anex 1800",
+            112,
+            "La FED 3 a été crée en 1967, c'est le premier reflex numerique. Il connait un succet fou dans les années 70' après que sa promotion dans une pub avec Mickael Jordan.",
+            "images/vcam_2.jpg");
+        let Camera3 = new Camera(
+            "name3",
+            "Camera Stramer 90'",
+            230,
+            "La FED 3 a été crée en 1967, c'est le premier reflex numerique. Il connait un succet fou dans les années 70' après que sa promotion dans une pub avec Mickael Jordan.",
+            "images/vcam_3.jpg");
+        let Camera4 = new Camera(
+            "name4",
+            "Camera Mamiya M",
+            99,
+            "La FED 3 a été crée en 1967, c'est le premier reflex numerique. Il connait un succet fou dans les années 70' après que sa promotion dans une pub avec Mickael Jordan.",
+            "images/vcam_4.jpg");
+        let Camera5 = new Camera(
+            "name5",
+            "Canon 310 M",
+            230,
+            "La FED 3 a été crée en 1967, c'est le premier reflex numerique. Il connait un succet fou dans les années 70' après que sa promotion dans une pub avec Mickael Jordan.",
+            "images/vcam_5.jpg");
+
+
+        console.log(Camera1);
+        console.log(Camera2);
+        console.log(Camera3);
+        console.log(Camera4);
+        console.log(Camera5);
+
+
+
+request.open("GET", "http://localhost:3000/api/cameras");
+
+// Transforme l'objet JavaScript en chaîne de caractères JSON
+    var texteCamera1 = JSON.stringify(Camera1);
+    var texteCamera2 = JSON.stringify(Camera2);
+    var texteCamera3 = JSON.stringify(Camera3);
+    var texteCamera4 = JSON.stringify(Camera4);
+    var texteCamera5 = JSON.stringify(Camera5);
+    console.log(texteCamera1);
+    console.log(texteCamera2);
+    console.log(texteCamera3);
+    console.log(texteCamera4);
+    console.log(texteCamera5);
+// Transforme la chaîne de caractères JSON en objet JavaScript
+    console.log(JSON.parse(texteCamera1));
+    console.log(JSON.parse(texteCamera2));
+    console.log(JSON.parse(texteCamera3));
+    console.log(JSON.parse(texteCamera4));
+    console.log(JSON.parse(texteCamera5));
 
 
